8cbd87dfa81bfcaca7c1f4d101abe197da65b901
Cbaleonsilva12 <bal24003@byui.edu>
2024-04-08 20:35:39 -0600

    I'm creating my final project The TaskManager

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,163 @@
 using System;
+using System.Collections.Generic;
+
+class Task
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Priority { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool Status { get; set; }
+
+    public void SetTitle(string title)
+    {
+        // Set the title of the task
+    }
+
+    public void SetDescription(string description)
+    {
+        // Set the description of the task
+    }
+
+    public void SetPriority(int priority)
+    {
+        // Set the priority of the task
+    }
+
+    public void SetDueDate(DateTime dueDate)
+    {
+        // Set the due date of the task
+    }
+
+    public void SetStatus(bool status)
+    {
+        // Set the status of the task
+    }
+
+    public void DisplayTaskDetails()
+    {
+        // Display the details of the task
+    }
+}
+
+class TaskManager
+{
+    public List<Task> Tasks { get; set; }
+
+    public TaskManager()
+    {
+        Tasks = new List<Task>();
+    }
+
+    public void AddTask(Task task)
+    {
+        // Add a task to the task list
+    }
+
+    public void RemoveTask(Task task)
+    {
+        // Remove a task from the task list
+    }
+
+    public void EditTask(Task oldTask, Task newTask)
+    {
+        // Edit an existing task
+    }
+
+    public Task GetTask(string title)
+    {
+        // Get a task by title
+        return null;
+    }
+
+    public List<Task> GetAllTasks()
+    {
+        // Get all tasks
+        return null;
+    }
+}
+
+class TaskViewer
+{
+    public void DisplayTaskList(List<Task> tasks)
+    {
+        // Display a list of tasks
+    }
+
+    public void DisplayTaskDetails(Task task)
+    {
+        // Display details of a specific task
+    }
+}
+
+class TaskEditor
+{
+    public Task CreateTask(string title, string description, int priority, DateTime dueDate)
+    {
+        // Create a new task
+        return null;
+    }
+
+    public void EditTaskDetails(Task task, string newTitle, string newDescription, int newPriority, DateTime newDueDate)
+    {
+        // Edit details of an existing task
+    }
+}
+
+class TaskSorter
+{
+    public List<Task> SortByPriority(List<Task> tasks)
+    {
+        // Sort tasks by priority
+        return null;
+    }
+
+    public List<Task> SortByDueDate(List<Task> tasks)
+    {
+        // Sort tasks by due date
+        return null;
+    }
+
+    public List<Task> SortByStatus(List<Task> tasks)
+    {
+        // Sort tasks by status
+        return null;
+    }
+}
+
+class TaskReminder
+{
+    public void SetReminder(Task task, DateTime reminderDateTime)
+    {
+        // Set a reminder for a task
+    }
+
+    public void CancelReminder(Task task)
+    {
+        // Cancel a reminder for a task
+    }
+}
+
+class TaskReporter
+{
+    public void GenerateTaskReport(List<Task> tasks)
+    {
+        // Generate a report based on tasks
+    }
+}
+
+class UserInputHandler
+{
+    public void HandleInput(string userInput)
+    {
+        // Handle user input and interact with other classes
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        // Program entry point
     }
 }
